56125cc5855f557a350904655c627bc87e83b087
erick alvear <erick7alvear@outlook.es>
2020-11-24 21:57:03 -0500

    Login&Menu

---
+++ WebApplication/Controllers/ConexionDTO/LoginDTO.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers.ConexionDTO
+{
+    public class LoginDTO
+    {
+        public Usuarios FunGetUsuarios(string user,string pass)
+        {
+            Usuarios _usuarios = new Usuarios();
+
+            try
+            {
+                using (BDD_HRVEntities _db = new BDD_HRVEntities())
+                {
+                    _usuarios = _db.Usuarios.Include("Perfiles").Where(u => u.Perfiles.estado_perfil & u.login_usuario == user
+                    && u.password_usuario == pass && u.estado_usuario).FirstOrDefault();
+                }
+
+                if (_usuarios == null) return null;
+                else return _usuarios;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
---
+++ WebApplication/Controllers/LoginController.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Mvc;
+using WebApplication.Controllers.ConexionDTO;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string user, string pass)
+        {
+            Usuarios _usu = new LoginDTO().FunGetUsuarios(user, pass);
+            if(_usu == null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Index", "Menu");
+            }
+        }
+    }
+}
