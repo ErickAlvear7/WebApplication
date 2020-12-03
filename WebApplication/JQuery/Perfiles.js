ec5c9cf55e26c56c82ccec41ae5b9487aac63ea2
erick alvear <erick7alvear@outlook.es>
2020-12-02 23:38:28 -0500

    cambios_varios_diseño

--- WebApplication/JQuery/Perfiles.js
+++ WebApplication/JQuery/Perfiles.js
@@ -4,7 +4,7 @@
         eve.preventDefault();
         $("#modal-content").load("/Perfiles/Create");
         $(".modal-title").text("Nuevo Perfil");
-        $("#header").css("background-color", "#85C1E9");
+        $("#header").css("background-color", "#2F90F3");
         $("#header").css("color", "white");
         _opcion = 0, _estado = true;
         _id = 0;
--- WebApplication/JQuery/Usuarios.js
+++ WebApplication/JQuery/Usuarios.js
@@ -4,7 +4,7 @@
         eve.preventDefault();
         $("#modal-content").load("/Usuarios/Create");
         $(".modal-title").text("Nuevo Usuario");
-        $("#header").css("background-color", "#85C1E9");
+        $("#header").css("background-color", "#2F90F3");
         $("#header").css("color", "white");
         _opcion = 0, _estado = true;
         _id = 0;
